b4bdadf98836ab8e4a3065f6d47d0367eb9a2ff9
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-12-28 17:45:27 +0000

    [ADD] database init functions to entrypoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
+from app.database import create_admin_user
+from app.database import create_db_and_tables
+from app.database import drop_db_and_tables
+
 app = FastAPI()
+
+@app.on_event("startup")
+def startup():
+    drop_db_and_tables()
+    create_db_and_tables()
+    create_admin_user()
 
 @app.get('/')
 def root():

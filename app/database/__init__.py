24ff44805cd3a04cf7870ae0f84dea6fc8c343b7
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-12-28 17:32:49 +0000

    [ADD] init functions to `app.database` module

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,2 +1,5 @@
 from app.database.admin_user import admin_user
 from app.database.engine import engine
+from app.database.init_database import create_admin_user
+from app.database.init_database import create_db_and_tables
+from app.database.init_database import drop_db_and_tables

c80326c50179cb95ab3c691c7179ccf49016e9c9
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-12-28 18:57:07 +0000

    [UPDATE] references to `first_admin`

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,4 +1,4 @@
-from app.database.admin_user import admin_user
+from app.database.first_admin import admin_user
 from app.database.engine import engine
 from app.database.init_database import create_admin_user
 from app.database.init_database import create_db_and_tables

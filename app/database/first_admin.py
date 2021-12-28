8df4fb4a23ab7c5914c19bba5dd3ced5e21a7dac
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-12-28 19:14:33 +0000

    [UPDATE] `first_admin` logic
    Now the data of first administrator are sourced from `.env` file.

--- app/database/first_admin.py
+++ app/database/first_admin.py
@@ -1,5 +1,16 @@
-admin_user = {
-    "role_id": 1,
-    "email": "super@admin.com",
-    "password": "superadmin",
-}
+from app.core import settings
+
+
+def get_first_admin_data():
+    """
+    Get first admin data from settings.
+    This data is used to create first admin user and are setted in `.env` file.
+    """
+
+    return {
+        "role_id": settings.FIRST_ADMIN_ROLE_ID,
+        "email": settings.FIRST_ADMIN_EMAIL,
+        "password": settings.FIRST_ADMIN_PASSWORD,
+    }
+
+superuser = get_first_admin_data()

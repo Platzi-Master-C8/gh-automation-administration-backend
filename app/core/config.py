62c953f539563daf0b046098d4ec89143f167d11
G.A.JAGUAR <dev.gajaguar@gmail.com>
2021-12-28 22:22:46 -0600

    [UPDATE] settings with new env variables

--- app/core/config.py
+++ app/core/config.py
@@ -7,9 +7,23 @@ load_dotenv()
 
 
 class Settings():
-    FIRST_ADMIN_ROLE_ID: str = os.getenv('FIRST_ADMIN_ROLE_ID')
-    FIRST_ADMIN_EMAIL: str = os.getenv('FIRST_ADMIN_EMAIL')
-    FIRST_ADMIN_PASSWORD: str = os.getenv('FIRST_ADMIN_PASSWORD')
+    # Application
+    APP_NAME = os.getenv("APP_NAME")
+    APP_PORT = os.getenv("APP_PORT")
+    APP_ENV = os.getenv("APP_ENV")
+
+    # Database
+    DB_DRIVER=os.getenv("DB_DRIVER")
+    DB_HOST=os.getenv("DB_HOST")
+    DB_PORT=os.getenv("DB_PORT")
+    DB_USER=os.getenv("DB_USER")
+    DB_PASSWORD=os.getenv("DB_PASSWORD")
+    DB_NAME=os.getenv("DB_NAME")
+
+    # First administrator
+    FIRST_ADMIN_ROLE_ID: str = os.getenv("FIRST_ADMIN_ROLE_ID")
+    FIRST_ADMIN_EMAIL: str = os.getenv("FIRST_ADMIN_EMAIL")
+    FIRST_ADMIN_PASSWORD: str = os.getenv("FIRST_ADMIN_PASSWORD")
 
 
 settings = Settings()

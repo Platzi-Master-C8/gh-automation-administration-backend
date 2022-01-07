9306c526daa20e2b31a2e998ab7eb56bcf55f942
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-07 07:11:58 +0000

    [UPDATE] `User` model to link with `Role`

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,18 +1,19 @@
-from typing import Optional
+from typing import Optional, TYPE_CHECKING
 
 from sqlalchemy import Column, String
-from sqlmodel import Field
+from sqlmodel import Field, Relationship
 
 from app.models import Auditor
+if TYPE_CHECKING:
+    from app.models import Role
 
 
 class User(Auditor, table=True):
     """
     Class representing a user in database.
     """
-
     user_id: Optional[int] = Field(default=None, primary_key=True)
-    role_id: Optional[int] = Field(default=2)
+    role_id: Optional[int] = Field(default=2, foreign_key="role.role_id")
     name: Optional[str] = Field(default=None)
     email: str = Field(
         sa_column=Column(
@@ -23,3 +24,4 @@ class User(Auditor, table=True):
         )
     )
     password: str = Field(..., max_length=64)
+    role: Optional["Role"] = Relationship(back_populates="users")

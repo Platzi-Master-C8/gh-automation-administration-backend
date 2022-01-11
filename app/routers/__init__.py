8400b32db1e022f51bb7a443d4bbbf00d3e8b747
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-11 05:56:27 +0000

    [UPDATE] reference to users router in `app.routers`

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,4 +1,4 @@
 __all__ = ["roles_router", "users_router"]
 from app.routers.base_router import configure
 from app.routers.roles_router import router as roles_router
-from app.routers.users_router import router as users_router
+from app.routers.users_router import users_router

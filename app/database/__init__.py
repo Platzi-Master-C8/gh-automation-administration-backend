e072be21d35cb18aac4e8b7f46879632a7095396
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-11 05:32:34 +0000

    [ADD] first roles to `app.database` module

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,4 +1,5 @@
 from app.database.first_admin import superuser
+from app.database.first_roles import roles
 from app.database.engine import engine
 from app.database.init_database import create_first_admin
 from app.database.init_database import create_db_and_tables

4d7eb9bbcb7e77ea380a0d3df881c5f4c9f84f07
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-12 18:06:38 +0000

    [CREATE] `Permission` model

---
+++ app/models/permission_model.py
@@ -0,0 +1,30 @@
+from typing import List, Optional, TYPE_CHECKING
+
+from sqlalchemy import Column, String
+from sqlmodel import Field, Relationship
+
+from app.models import Auditor
+
+if TYPE_CHECKING:
+    from app.models import Role
+    from app.models import RoleHasPermission
+
+
+class Permission(Auditor, table=True):
+    """
+    Class representing a permission in database.
+    """
+    permission_id: Optional[int] = Field(default=None, primary_key=True)
+    permission_name: str = Field(
+        ...,
+        sa_column=Column(
+            "permission_name",
+            String(50),
+            unique=True,
+        )
+    )
+    permission_description: Optional[str] = Field(default=False)
+    roles: List["Role"] = Relationship(
+        back_populates="permissions",
+        link_model=RoleHasPermission,
+    )

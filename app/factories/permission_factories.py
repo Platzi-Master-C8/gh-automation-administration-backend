d3856e35f25e6ea4d6df07db008119e9c18fc3de
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-15 16:46:04 +0000

    [FIX] `_get_permission_name` in `permission_factories`
    Now convert resulting strings to lowercase and strip trailing period.

--- app/factories/permission_factories.py
+++ app/factories/permission_factories.py
@@ -6,26 +6,26 @@ from app.models import Permission
 
 fake = Faker()
 
-def _get_permission_name():
+def _get_permission_name() -> str:
     """
     Generate a random permission name.
     The structure of the name is:
     `can-{read|write}-{any|own}-{resource}`
     """
     action = fake.sentence(
-        nb_words=2,
+        nb_words=1,
         variable_nb_words=False,
         ext_word_list=["read", "write"]
-    )
+    ).lower().strip(".")
     scope = fake.sentence(
-        nb_words=2,
+        nb_words=1,
         variable_nb_words=False,
         ext_word_list=["any", "own"]
-    )
+    ).lower().strip(".")
     resource = fake.sentence(
         nb_words=1,
         variable_nb_words=False,
-    )
+    ).lower().strip(".")
     permission_name = f"can-{action}-{scope}-{resource}"
     return permission_name
     
@@ -41,5 +41,5 @@ class RandomPermissionFactory(factory.Factory):
         model = Permission
 
 
-    permission_name = factory.LazyFunction(_get_permission_name())
+    permission_name = factory.LazyFunction(_get_permission_name)
     permission_description = factory.Faker("text", max_nb_chars=80)

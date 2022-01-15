5cb2a8b117d3d2f205f272f7ca6c23bedde61fe5
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-15 07:17:50 +0000

    [CREATE] `user_factories`

---
+++ app/database/factories/user_factories.py
@@ -0,0 +1,39 @@
+import factory
+
+from app.core import settings
+from app.models import User
+
+
+class SuperadminUserFactory(factory.Factory):
+    """
+    Class representing a superadmin user factory.
+    Take data from settings and create admin user.
+    """
+    class Meta:
+        model = User
+
+    
+    name = settings.FIRST_ADMIN_ROLE_ID
+    email = settings.FIRST_ADMIN_EMAIL
+    password = settings.FIRST_ADMIN_PASSWORD
+    
+    
+    
+class RandomUserFactory(factory.Factory):
+    """
+    Class representing a random user factory.
+    Use random data to create random user.
+    By default password is fixed to `password`.
+    """
+    class Meta:
+        model = User
+
+
+    name = factory.Faker("name")
+    email = factory.Faker("free_email")
+    password = "password"
+    role_id = factory.Faker(
+        "pyint",
+        min_value=1,
+        max_value=settings.DB_POPULATION
+    )

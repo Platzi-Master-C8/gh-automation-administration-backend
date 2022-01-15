bc88b797f50f8b0f18c2a0e8e475d0eecb3b053d
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-15 02:08:58 +0000

    [UPDATE] `auth_router` to implement new exception
    Delete `incorrect_credentials` logic an convert it into a response
    class.

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,27 +1,23 @@
 from fastapi import Depends, APIRouter, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.utils import create_access_token, verify_password
 from app.resources import users
+from app.responses import MismatchCredentialsExeption
+from app.utils import create_access_token, verify_password
+
 
 router = APIRouter(
     prefix='/auth',
     tags=['auth']
 )
 
-incorrect_credentials = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Incorrect username or password",
-    headers={"WWW-Authenticate": "Bearer"},
-)
-
 @router.post('/login')
 def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = users.get_by_email(form_data.username)
     if not user:
-        raise incorrect_credentials
+        raise MismatchCredentialsExeption()
     if not verify_password(form_data.password, user.password):
-        raise incorrect_credentials
+        raise MismatchCredentialsExeption()
     data = {
         'sub': user.email,
         'role_id': user.role_id

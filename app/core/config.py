9e516c85adb316bec7f5d37ed24655c297eccc45
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-15 05:56:44 +0000

    [ADD] new environment variables to `settings`
    - DB_RESET
    - DB_POPULATE
    - DB_POPULATION

--- app/core/config.py
+++ app/core/config.py
@@ -19,6 +19,9 @@ class Settings():
     DB_USER=os.getenv("DB_USER")
     DB_PASSWORD=os.getenv("DB_PASSWORD")
     DB_NAME=os.getenv("DB_NAME")
+    DB_RESET=os.getenv("DB_RESET")
+    DB_POPULATE=os.getenv("DB_POPULATE")
+    DB_POPULATION=os.getenv("DB_POPULATION")
 
     # First administrator
     FIRST_ADMIN_ROLE_ID: str = os.getenv("FIRST_ADMIN_ROLE_ID")

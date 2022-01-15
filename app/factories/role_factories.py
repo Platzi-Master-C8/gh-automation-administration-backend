3a7f634f5fde13c50706210773ae0d55293250a5
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-15 16:41:42 +0000

    [UPDATE] `role_factories` by commenting code lines
    Certain features causes an error.

--- app/factories/role_factories.py
+++ app/factories/role_factories.py
@@ -1,31 +1,30 @@
 import factory
 from faker import Faker
 
-from app.core import settings
+# from app.core import settings
+# from app.factories import RandomPermissionFactory
 from app.models import Role
-from app.factories import RandomPermissionFactory
 
 
 fake = Faker()
+# population = settings.DB_POPULATION
 
-random_permissions = RandomPermissionFactory().build_batch(
-    settings.DB_POPULATION
-)
-"""
-Group of instances of the `Permission` model.
-"""
+# random_permissions = RandomPermissionFactory().build_batch(population)
+# """
+# Group of instances of the `Permission` model.
+# """
 
-def _get_permissions():
-    """
-    Create a set of random permissions.
-    """
-    max_value = len(random_permissions)
-    permissions = set()
-    number_of_permissions = fake.pyint(1, max_value)
-    for _ in range(number_of_permissions):
-        index = fake.pyint(max_value - 1)
-        permissions.add(random_permissions[index])
-    return permissions
+# def _get_permissions()->set:
+#     """
+#     Create a set of random permissions.
+#     """
+#     max_value = len(random_permissions)
+#     permissions = set()
+#     number_of_permissions = fake.pyint(1, max_value)
+#     for _ in range(number_of_permissions):
+#         index = fake.pyint(max_value - 1)
+#         permissions.add(random_permissions[index])
+#     return permissions
 
 
 class RandomRoleFactory(factory.Factory):
@@ -39,4 +38,4 @@ class RandomRoleFactory(factory.Factory):
 
     role_name = factory.Faker("word")
     role_description = factory.Faker("text", max_nb_chars=80)
-    permissions = factory.LazyFunction(_get_permissions)
+    # permissions = factory.LazyFunction(_get_permissions())

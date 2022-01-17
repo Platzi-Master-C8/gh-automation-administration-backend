3990abb06e0ee5eac6c50b4a5401fbf6e9260607
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-17 14:42:18 +0000

    [ADD] first roles factories to `app.factories` module

--- app/factories/__init__.py
+++ app/factories/__init__.py
@@ -1,4 +1,7 @@
 from app.factories.permission_factories import RandomPermissionFactory
+from app.factories.role_factories import AdministratorRoleFactory
+from app.factories.role_factories import IndividualRoleFactory
+from app.factories.role_factories import OrganizationRoleFactory
 from app.factories.role_factories import RandomRoleFactory
 from app.factories.user_factories import RandomUserFactory
 from app.factories.user_factories import SuperadminUserFactory

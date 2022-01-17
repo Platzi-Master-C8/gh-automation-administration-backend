94d0bf0495036b2e48919466b0a251de478a32ce
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-17 14:41:44 +0000

    [CREATE] first roles factories

--- app/factories/role_factories.py
+++ app/factories/role_factories.py
@@ -27,6 +27,40 @@ fake = Faker()
 #     return permissions
 
 
+class AdministratorRoleFactory(factory.Factory):
+    """
+    Class representing the administrator role.
+    """
+    class Meta:
+        model = Role
+
+        
+    role_name = "administrator"
+    role_description = "Grant total control over the application."
+
+
+class IndividualRoleFactory(factory.Factory):
+    """
+    Class representing the individual role.
+    """
+    class Meta:
+        model = Role
+
+    role_name = "individual"
+    role_description = "Standard user with limited permissions."
+
+
+class OrganizationRoleFactory(factory.Factory):
+    """
+    Class representing the organization role.
+    """
+    class Meta:
+        model = Role
+
+    role_name = "organization"
+    role_description = "Oriented to talent searchers"
+
+
 class RandomRoleFactory(factory.Factory):
     """
     Class representing a random role factory.

4a43b321ed0f9385a98632b768c3f85547bc7abd
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-18 13:37:14 -0600

    [UPDATE] `init_database` to implement reset functions

--- app/database/init_database.py
+++ app/database/init_database.py
@@ -1,21 +1,19 @@
-from sqlmodel import SQLModel
-
 from app.core import settings
-from app.models import *
-from app.database import engine
 from app.database.populate import populate_permissions
 from app.database.populate import populate_roles
 from app.database.populate import populate_users
+from app.database.reset import crate_all_tables
+from app.database.reset import drop_all_tables
+from app.models import *
+
 
 def init_database()->None:
     """
     Run the database initialization functions according to the settings.
     """
     if settings.DB_RESET:
-        SQLModel.metadata.drop_all(engine)
-        print("DEBUG:   ", "Database and tables dropped.")
-        SQLModel.metadata.create_all(engine)
-        print("DEBUG:   ", "Database and tables created.")
+        drop_all_tables()
+        crate_all_tables()
     if settings.DB_POPULATE:
        populate_permissions(settings.DB_POPULATION) 
        populate_roles(settings.DB_POPULATION) 

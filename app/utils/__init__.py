88afaef7eda147bfd1fb1bc42c633aa42089059d
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-21 03:28:48 +0000

    [UPDATE] `permission_factories` to clean code
    Extract `get_permission_name` function from factory and move it to
    `app.utils` module.

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,4 @@
+from app.utils.get_permission_name import get_permission_name
 from app.utils.hash_password import hash_password
 from app.utils.to_dict import to_dict
 from app.utils.token import create_access_token

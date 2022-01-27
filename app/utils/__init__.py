faad395a2166e319ad00480030b8af6a24b9006c
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-27 19:45:26 +0000

    [ADD] `get_verified_token` to `app.utils` module

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,6 +1,7 @@
 from app.utils.get_permission_name import get_permission_name
 from app.utils.hash_password import hash_password
 from app.utils.get_response_login import get_response_login
+from app.utils.get_verified_token import get_verified_token
 from app.utils.to_dict import to_dict
 from app.utils.create_access_token import create_access_token
 from app.utils.get_payload import get_payload

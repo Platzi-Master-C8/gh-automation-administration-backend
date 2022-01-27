7eee2941ffcf7f3d31a61813d9ccd05d7f9eb7e8
G.A.JAGUAR <dev.gajaguar@gmail.com>
2022-01-27 17:01:30 +0000

    [UPDATE] auth router to improve its functionality
    Add `user_id` to token payload.

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,25 +1,37 @@
-from fastapi import Depends, APIRouter, HTTPException, status
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.resources import users
 from app.responses import MismatchCredentialsExeption
-from app.utils import create_access_token, verify_password
+from app.schemas import Token
+from app.utils import create_access_token
+from app.utils import get_response_login
+from app.utils import verify_password
 
 
 router = APIRouter(
-    tags=['auth'],
+    tags=["auth"],
 )
 
-@router.post('/token')
-async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+@router.post("/token", status_code=status.HTTP_200_OK, response_model=Token)
+async def login(form_data: OAuth2PasswordRequestForm = Depends()) -> dict:
+    """
+    Provide access token for user.
+    Must receive `username` and `password`  into a `x-www-form-urlencoded`
+    format to return a JWT token.
+    """
     user = users.get_by_email(form_data.username)
     if not user:
         raise MismatchCredentialsExeption()
     if not verify_password(form_data.password, user.password):
         raise MismatchCredentialsExeption()
     data = {
-        'sub': user.email,
-        'role_id': user.role_id,
+        "sub": user.email,
+        "user_id": user.user_id,
+        "role_id": user.role_id,
     }
     access_token = create_access_token(data)
-    return {"access_token": access_token, "token_type": "bearer"}
+    response_login = get_response_login(access_token)
+    return response_login 

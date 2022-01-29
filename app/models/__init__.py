f4be66d04521d3236c57d5187b0d2b4e873d24c4
WToloza <wtorrestoloza@hotmail.com>
2022-01-29 18:02:31 -0500

    [EDIT] import link table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ["Permission", "Role", "RolePermission", "User"]
+__all__ = ["Permission", "Role", "RolePermissionLink", "User"]
 from app.models.auditor_model import Auditor
 from app.models.models import Permission
-from app.models.models import RolePermission
+from app.models.models import RolePermissionLink
 from app.models.models import Role
 from app.models.models import User

367ad1742d119631384e5b3179ffafd7aafc7c22
WToloza <wtorrestoloza@hotmail.com>
2022-01-29 18:07:52 -0500

    [EDIT] for create string by map function with lambda

--- app/utils/get_permissions_as_string.py
+++ app/utils/get_permissions_as_string.py
@@ -7,5 +7,5 @@ def get_permissions_as_string(permissions: List[Permission]) -> str:
     """
     Convert a list of permissions to a string.
     """
-    permissions_string = "".join(map(str, permissions))
+    permissions_string = "".join(map(lambda x: str(x.permission_name), permissions))
     return permissions_string
